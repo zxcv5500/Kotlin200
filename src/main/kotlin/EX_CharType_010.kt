234c36f678739c7bda9fae0f05c1059ac47d8330
pangyo255 <pangyo255@gmail.com>
2020-11-08 03:26:28 +0900

    기존 실습 코드에 넘버링 추가함.

---
+++ src/main/kotlin/EX_CharType_010.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var ch: Char = 'A'
+    println(ch)
+
+    ch = '\uAC00'
+    println(ch)
+
+    ch = '한'
+    println(ch.toInt())
+}
---
+++ src/main/kotlin/EX_trapOfRealNumber_009.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    println(0.1f + 0.1f + 0.1f)
+    println(0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f)
+    println(0.1f * 10)
+    println(0.1f * 10)
+}
