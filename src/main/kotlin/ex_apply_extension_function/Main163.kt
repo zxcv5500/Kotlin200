f7fe0bad48bef6d0ee0f174fb7f4aac7224deb68
pangyo255 <pangyo255@gmail.com>
2020-12-01 08:03:44 +0900

    166 BigInteger까지 코드 사용해봄

---
+++ src/main/kotlin/ex_apply_extension_function/Main163.kt
@@ -0,0 +1,17 @@
+package ex_apply_extension_function
+
+class Person
+{
+    var name: String = ""
+    var age: Int = 0
+    var money: Int = 0
+
+    override fun toString(): String = "$name $age $money"
+}
+
+fun main(args: Array<String>) {
+    println(Person().apply {
+        this.name = "Alan"
+        this.money = 70
+    })
+}
